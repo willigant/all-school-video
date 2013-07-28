0fbe4f21f6f4cdcc94201acbd837b7fddc3333a8
Khaliq Gant <khaliqgant@gmail.com>
2013-07-28 12:55:35 -0400

    hook up views and play some damn videos

--- laravel/app/views/videos.php
+++ laravel/app/views/videos.php
@@ -6,10 +6,14 @@ Videos
 <?= $standard->exercise_url;?>
 
 
-<? foreach ($videos as $video) { ?>
+<? foreach ($videos as $video) { 
+    //need travis to input just the youtube id and not the full URL
+    //but for now process the video and just pull out the video id
+    $video_id = str_replace("http://www.youtube.com/watch?v=","",$video->url);
+?>
 
-    <?= $video->url; ?>
-    <?= $video->id; ?>
+    <iframe width="560" height="315" src="http://www.youtube.com/embed/<?= $video_id; ?>" 
+        frameborder="0" allowfullscreen>
+    </iframe>
 
-<?
-}
+<? } ?>
