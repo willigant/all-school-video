b6fa25e29173fae2d5b433e8a989511da856ac30
Khaliq Gant <khaliqgant@gmail.com>
2013-07-28 00:26:54 -0400

    add main layout file

---
+++ layout.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+  <title>Standards Page</title>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
+
+  <!-- mobile viewport optimisation -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- stylesheets -->
+  <link href="stylesheets/_normalize.css" rel="stylesheet" type="text/css" />
+  <link href="stylesheets/app.css" media="screen, projector, print" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <!--content-->
+</body>
+</html>
